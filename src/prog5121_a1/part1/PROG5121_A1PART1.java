6ed310dad527c1f311d4beb218b7f229e6278685
St10453122 <ST10453122@rcconnect.edu.za>
2025-04-14 11:07:26 +0200

    Fixed  a few issues
    Final Final final commit

--- src/prog5121_a1/part1/PROG5121_A1PART1.java
+++ src/prog5121_a1/part1/PROG5121_A1PART1.java
@@ -1,128 +1,115 @@
 package prog5121_a1.part1;
+
+import java.util.Scanner;
+
 /**
  *
  * @author ST10453122 Silvestre Firmino
  */
-import java.util.Scanner;
-
 public class PROG5121_A1PART1 {
 
     public static void main(String[] args) {
         VerifyUser registeredUser = registerUser();
         boolean loginSuccess = loginUser(registeredUser);
-
         returnLoginStatus(loginSuccess, registeredUser);
     }
 
     public static VerifyUser registerUser() {
         Scanner check = new Scanner(System.in);
-        String firstName = "";
-        String lastName = "";
+        VerifyUser tempUser = new VerifyUser(); // Fixed constructor call
+
         String username = "";
         String password = "";
         String cellphoneNumber = "";
 
-        VerifyUser tempUser = new VerifyUser("", "", "","","");
-
         System.out.println("************ User Registration *************");
 
-        System.out.println("Enter your First name:>> ");
-        firstName = check.nextLine();
-        
-        System.out.println("Enter your Last name:>> ");
-        lastName = check.nextLine();
-        
+        System.out.print("Enter your First name:>> ");
+        String firstName = check.nextLine();
+        tempUser.setFirstName(firstName);
+
+        System.out.print("Enter your Last name:>> ");
+        String lastName = check.nextLine();
+        tempUser.setLastName(lastName);
+
         // Username
-        //The whlie loop ,loops until the conditions are met, through the checkUserName 
         while (true) {
-            System.out.println("Enter your Username (must contain _ and be no more than 5 characters):>> ");
+            System.out.print("Enter your Username (must contain _ and be no more than 5 characters):>> ");
             username = check.nextLine();
-            //validate the user input if the username meets the conditions of the correct username format
-            if (tempUser.checkUserName(username)){ 
-               System.out.println("Username successfully captured.");
-               //ends the loop when true
-               break;
-        } else {
-            System.out.println("Username is not correctly formatted, please ensure it contains an underscore and is no more than five characters.");
-        }
+            if (tempUser.checkUserName(username)) {
+                System.out.println("Username successfully captured.");
+                break;
+            } else {
+                System.out.println("Username is not correctly formatted, please ensure it contains an underscore and is no more than five characters.");
+            }
         }
 
         // Password
-
         while (true) {
-            System.out.println("Enter your Password (at least 8 characters, one uppercase, one number, one special character):>> ");
+            System.out.print("Enter your Password (at least 8 characters, one uppercase, one number, one special character):>> ");
             password = check.nextLine();
-            if (tempUser.checkPasswordComplexity(password)){
-                    System.out.println("Password successfully captured.");
-                    break;
-                
+            if (tempUser.checkPasswordComplexity(password)) {
+                System.out.println("Password successfully captured.");
+                break;
             } else {
                 System.out.println("Password is not correctly formatted. Try again.");
             }
-            }
-            
-        // Cellphone
-      
+        }
+
+        // Cellphone Number
         while (true) {
-            System.out.println("Enter your South African cellphone number (+27xxxxxxxxx):>> ");
+            System.out.print("Enter your South African cellphone number (+27xxxxxxxxx):>> ");
             cellphoneNumber = check.nextLine();
-            if (tempUser.checkCellPhoneNumber(cellphoneNumber)){
-                 System.out.println("Cell phone number successfully added.");
-                 break;
+            if (tempUser.checkCellPhoneNumber(cellphoneNumber)) {
+                System.out.println("Cell phone number successfully added.");
+                break;
             } else {
                 System.out.println("Cell phone number incorrectly formatted or does not contain international code.");
-            
             }
+        }
 
-    }
         System.out.println("********************************************");
-        //storing the input values into these variables
-        return new VerifyUser(username, password, cellphoneNumber, firstName, lastName);
- }
 
+        return new VerifyUser(username, password, cellphoneNumber, firstName, lastName);
+    }
 
     public static boolean loginUser(VerifyUser registeredUser) {
         Scanner check = new Scanner(System.in);
-        //providing attempts for 3 chances to login into your account
         int attempts = 0;
 
         System.out.println("\n*************** User Login ***************");
 
         while (attempts < 3) {
-            System.out.println("Enter your Username:>> ");
+            System.out.print("Enter your Username:>> ");
             String enteredUsername = check.nextLine();
 
-            System.out.println("Enter your Password:>> ");
+            System.out.print("Enter your Password:>> ");
             String enteredPassword = check.nextLine();
 
-            System.out.println("Enter your Cellphone Number:>> ");
+            System.out.print("Enter your Cellphone Number:>> ");
             String enteredCell = check.nextLine();
-            // calls the boolean authenticate method from verify user class to check if all login inputs are matching register inputs
+
             if (registeredUser.authenticateUser(enteredUsername, enteredPassword, enteredCell)) {
                 return true;
             } else {
-                //loops twice until you match your vaild details and decrements everytime you get it wrong and stops the program
                 System.out.println("Incorrect details. Attempts left:>> " + (2 - attempts));
-                System.out.println("Maximum login attempts reached.");
                 attempts++;
             }
         }
 
+        System.out.println("Maximum login attempts reached.");
         return false;
     }
 
     public static String returnLoginStatus(boolean loginSuccess, VerifyUser user) {
-        VerifyUser tempUser = new VerifyUser("", "", "","","");
         System.out.println("********************************************");
-        //when vaild, prints out this result
         if (loginSuccess) {
             System.out.println("Login successful!");
-            System.out.println("Welcome "+ tempUser.getFirstName()+ " " + tempUser.getLastName() +  "it is great to see you again");
+            System.out.println("Welcome " + user.getFirstName() + " " + user.getLastName() + ", it is great to see you again!");
             return "Login successful!";
         } else {
-            //Prints out this result when attempts reach maximum attempts
             System.out.println("Login failed!");
-            System.err.println("Username or password incorrect , please try again");
+            System.err.println("Username or password incorrect, please try again.");
             return "Login failed.";
         }
     }
--- src/prog5121_a1/part1/VerifyUser.java
+++ src/prog5121_a1/part1/VerifyUser.java
@@ -1,5 +1,5 @@
-
 package prog5121_a1.part1;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,46 +14,73 @@ public class VerifyUser {
     private String username;
     private String password;
     private String cellPhoneNumber;
- 
-    //contractor
-    public VerifyUser(String lastName ,String firstName ,String username, String password , String cellPhoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+
+    // Constructor
+    public VerifyUser(String username, String password, String cellPhoneNumber, String firstName, String lastName) {
         this.username = username;
         this.password = password;
         this.cellPhoneNumber = cellPhoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
+    // No-arg constructor (to allow creating a temp user object for validation)
+    public VerifyUser() {
+        // No values initialized yet
+    }
+
+    // Username validation
     public boolean checkUserName(String username) {
-        /* returns true if username is correctly formatted */
+        // Must contain underscore and be 5 characters or less
         return username.contains("_") && username.length() <= 5;
     }
 
+    // Password validation
     public boolean checkPasswordComplexity(String password) {
-        /* returns true if password is correctly formatted  and contains regex values*/
+        // At least 8 characters, one uppercase, one lowercase, one digit, one special character
         String regex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{8,20}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
 
+    // Cellphone validation for South African format (+27 followed by 9 digits)
     public boolean checkCellPhoneNumber(String cellPhone) {
-        /* returns true if phone number is correctly formatted and if the phone number is South African */
-        
-        String regex = ("^\\+27\\d{9}$");
+        String regex = "^\\+27\\d{9}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(cellPhone);
         return matcher.matches();
-
     }
 
-    public boolean authenticateUser(String enteredUsername, String enteredPassword , String enteredCellPhoneNumber) {
-        /* returns true if username, password and cellphone number are correct */
-        return this.username.equals(enteredUsername) && this.password.equals(enteredPassword) && this.cellPhoneNumber.equals(enteredCellPhoneNumber);
+    //Authenticate login
+    public boolean authenticateUser(String enteredUsername, String enteredPassword, String enteredCellPhoneNumber) {
+        return this.username.equals(enteredUsername)
+            && this.password.equals(enteredPassword)
+            && this.cellPhoneNumber.equals(enteredCellPhoneNumber);
     }
 
-    
-    //Getter values returned to print out the login success 
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCellPhoneNumber(String cellPhoneNumber) {
+        this.cellPhoneNumber = cellPhoneNumber;
+    }
+
+    //Getters
     public String getUsername() {
         return username;
     }
@@ -61,17 +88,12 @@ public class VerifyUser {
     public String getCellPhoneNumber() {
         return cellPhoneNumber;
     }
-    
-    
-    public String getFirstName(){
-     
-        return firstName;
 
-}
-    
-    public String getLastName(){
-    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
         return lastName;
     }
 }
-
